f9fa028ae1fa89000248a9fb9f0c492c276d0ac2
Bet M <sbm2391@hotmail.com>
2017-10-09 13:10:35 -0700

    slideshow

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,8 +1,10 @@
+//var burguer nav
 var btn = document.getElementById("bars");
 var menu = document.getElementById("mini-nav-il");
 var bool = true;
+// var slider
 
-
+//burguer nav
 function displayMenu() {
     if (bool) {
       menu.setAttribute("id", "mostrar")
@@ -17,3 +19,18 @@ function displayMenu() {
 }
 
 btn.addEventListener("click", displayMenu, bool);
+
+//slideshow
+
+ var actual = 0;
+ funcion mostrar(n) {
+   var item = document.getElementsbyClassName("item");
+   for(1 = 0; i< item.length; i++) {
+     if(item[i].className.includes("actual")) {
+       item[i].className = item[i].className.replace("actual", "");
+       break;
+     }
+   }
+   actual = n;
+   item[n].className += " actual";
+ }
