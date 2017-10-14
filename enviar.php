be0138a725836636663b72898cde7ad64671813e
vavelar <victor.avelar@trivago.com>
2017-10-14 21:55:34 +0200

    ......

--- enviar.php
+++ enviar.php
@@ -3,7 +3,7 @@
 require_once('vendor/autoload.php');
 
   SimpleMail::make()
-    ->setTo("sbm2391@hotmail.com", 'Contacto desde la web del '.  date())
+    ->setTo("sbm2391@hotmail.com", 'Contacto desde la web del '.  date('d-m-Y'))
     ->setFrom($_POST['correo'], $_POST['nombre'])
     ->setSubject($_POST['asunto'])
     ->setMessage($_POST['mensaje'])
